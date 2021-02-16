bdad20e28f21418b99a3bd838593091ea145d821
Whitley Darby <64851791+whitleybhdarby@users.noreply.github.com>
2021-02-16 13:27:58 -0500

    placed a few icons and javascript

--- whitley_darby_stories_as_network_project_2/js/global.js
+++ whitley_darby_stories_as_network_project_2/js/global.js
@@ -1,7 +1,16 @@
 window.addEventListener("load",function() {
    
-    
+    const open = document.getElementsByClassName ('.open');
+    const modal_container = document.getElementsByClassName ('.modal-container');
+    const close = document.getElementsByClassName ('.close');
 
+    open.addEventListener('click',() => {
+        modal_container.classList.add('show');
+    })
+
+    close.addEventListener('click',() => {
+        modal_container.classList.remove('show');
+    })
     
 
 
