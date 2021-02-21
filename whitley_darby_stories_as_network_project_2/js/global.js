975a4247211722db6f6d68ab463345d16c6288e8
Whitley Darby <64851791+whitleybhdarby@users.noreply.github.com>
2021-02-20 22:40:34 -0500

    tried to figure out Javascript again

--- whitley_darby_stories_as_network_project_2/js/global.js
+++ whitley_darby_stories_as_network_project_2/js/global.js
@@ -1,21 +1,17 @@
-window.addEventListener("load",function() {
+window.addEventListener("load", function () {
 
-    var open = document.querySelectorAll('.fas fa-comment fa-lg')
+    const open = document.getElementsByClassName('.svg-inline--fa');
+    const modalContainer = document.getElementsByClassName('.modalContainer');
+    const close = document.getElementsByClassName('.close');
 
+    open(0).addEventListener('click', () => {
+        modalContainer.classList.add('show');
+    })
 
+    close(1).addEventListener('click', () => {
+        modalContainer.classList.remove('show');
+    })
 
-    // const open = document.getElementsByClassName ('.fas fa-comment fa-lg');
-    // const modalContainer = document.getElementsByClassName ('.modalContainer');
-    // const close = document.getElementsByClassName ('.close');
-
-    // open.addEventListener('click',() => {
-    //     modalContainer.classList.add('show');
-    // })
-
-    // close.addEventListener('click',() => {
-    //     modalContainer.classList.remove('show');
-    // })
-    
 
 
 })
