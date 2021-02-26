dcb9e85f575b26a2dc766e74aab74fd65d658c99
Whitley Darby <64851791+whitleybhdarby@users.noreply.github.com>
2021-02-26 13:29:08 -0500

    javascript intro

---
+++ whitley-darby-javascript-intro/js/main (Conflicted copy from Whitley's MacBook Pro on 2021-02-26).js
@@ -0,0 +1,7 @@
+console.log('Whitley Darby');
+let number = 4;
+if(number = 2 + 2){
+    console.log('true');
+}
+console.log('4');
+let isThisMakingSense = true;
---
+++ whitley-darby-javascript-intro/js/main.js
@@ -0,0 +1,70 @@
+var name = 'Whitley Darby';
+console.log('name');
+ var myFavNumber = 4;
+console.log('myFavNumber');
+var anxious = ('true');
+console.log('anxious');
+var lettersOfName = (['w','h','i','t','l','e','y']);
+console.log(lettersOfName);
+var differentData = ([3 + 5,'boston',false]);
+console.log(differentData);
+var firstName = ('Whitley');
+var lastName = ('Darby');
+var nameTogether = firstName.concat(lastName);
+console.log(nameTogether);
+var equalOneHundred = 50 + 50;
+console.log(equalOneHundred);
+console.log(lettersOfName.length);
+var maryPoppins = (['s','u','p','e','r','c','a','l','i','f','r','a','g','i','l','i','s','t','i','c','e','x','p','i','a','l','i','d','o','c','i','o','u','s']);
+console.log(maryPoppins.length);
+var fullNameCal = 7 + 5;
+console.log(fullNameCal);
+for (var fullNameCal = 0; fullNameCal < 12; fullNameCal++) {
+  console.log(fullNameCal)
+  }
+
+var time = new Date() .getHours();
+if (time < 7){
+    message = 'not quite it is 6:00PM';
+} else if (time < 9) {
+    message = 'correct it is 8:00PM';
+}
+console.log(time);
+
+var time = new Date() .getHours();
+if (time < 7){
+    message = 'not quite, time is 6:00PM';
+} else if (time < 9) {
+    message = 'correct it is 8:00PM';
+}
+console.log(time);
+
+var date = new Date();
+if (date < 25){
+    message = 'Friday Baby';
+} else if (date < 29) {
+    message = 'March it is';
+}
+console.log(date);
+var favRestaurants = (['Ihop','Red Lobster','Sonic']);
+console.log(favRestaurants);
+var canNotDo = (['walk without mask','face to face contact','vacation']);
+console.log(canNotDo);
+var nycPlaces = (['Utopia Nails','Krave it Pizza','Roosevelt Field Mall']);
+console.log(nycPlaces);
+var favMovies = (['Avatar','The Great Gatsby','Oliver and Company'])
+console.log(favMovies);
+
+for (var favRestaurants = 0; favRestaurants < 3; favRestaurants++) {
+    console.log(favRestaurants)
+    }
+for (var canNotDo = 0; canNotDo < 3; canNotDo++) {
+        console.log(canNotDo)
+    }
+for (var nycPlaces = 0; nycPlaces < 3; nycPlaces++) {
+        console.log(nycPlaces)
+    }
+for (var favMovies = 0; favMovies < 3; favMovies++) {
+        console.log(favMovies)
+    }
+console.log('could not quite get how do it. Looked everywhere to understand');
