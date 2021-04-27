6ea1bd040d4ac7281edd9e4cf0075fac4ed6a60e
Whitley Darby <64851791+darbw698@users.noreply.github.com>
2021-04-27 15:50:07 -0400

    did something

--- whitley_darby_input-output_project-4/js/global.js
+++ whitley_darby_input-output_project-4/js/global.js
@@ -7,30 +7,43 @@ console.log(backButton);
 var newEntry = document.getElementById("add-button");
 console.log(newEntry);
 
-<<<<<<< Updated upstream
-about.addEventListener("click", () => {
-      document.getElementById("container").style.display = "block";
-      document.getElementById("container").scrollTop = 0;
-=======
 // about.addEventListener("click", () => {
 //       document.getElementById("container").style.display = "block";
 //       document.getElementById("container").scrollTop = 0;
-
+//
 // });
-
+//
 // home.addEventListener("click", () => {
 //     document.getElementById("container").style.display = "none";
 //     document.getElementById("container").scrollTop = 0;
 // });
->>>>>>> Stashed changes
-
-});
-
-home.addEventListener("click", () => {
-    document.getElementById("container").style.display = "none";
-    document.getElementById("container").scrollTop = 0;
-});
 
 newEntry.addEventListener("click", () => {
-
+  const entry = createDiaryEntry({
+    color: document.documentElement.dataset.diaryColor
+  });
+  document.querySelector('.grid2').appendChild(entry);
+  entry.querySelector('.textarea').focus();
 });
+
+function createDiaryEntry({ color = 'pink' } = {}) {
+  const container = document.createElement('div');
+  container.className = `text-entry text-entry-${color}`;
+  const title = document.createElement('p');
+  title.className = 'diary-title';
+  title.textContent = 'Dear Covid Diaries,';
+  const textarea = document.createElement('textarea');
+  textarea.className = 'textarea';
+  textarea.setAttribute('contenteditable', '');
+  const submitButton = document.createElement('button');
+  submitButton.className = 'submit-button';
+  submitButton.textContent = 'Submit';
+
+  submitButton.addEventListener('click', () => {
+    alert('Submitted');
+  });
+
+  container.append(title, textarea, submitButton);
+
+  return container;
+}
